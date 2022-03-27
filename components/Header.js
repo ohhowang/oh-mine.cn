2d2ba2abf922faf56ca235f8e1fcf3da1f163724
oh-mine <mr.wang214880676@gmail.com>
2022-03-27 22:14:25 +0800

    feat: update something

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import icon from './icon.gif';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4">
+        {/* <Image
+          className="mt-10"
+          src={icon.src}
+          alt="halo's icon"
+          width={51}
+          height={36}
+        /> */}
+      </div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Halo';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : '写点儿东西';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '暂时不知道些什么，先空着。';
 
   return {
     name,
